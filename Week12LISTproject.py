5258e33992ebaf6ec77e0f05f3101abda09eeda8
Chad Bates <cbates255@gmail.com>
2022-08-24 11:52:42 -0400

    Completed project code

--- Week12LISTproject.py
+++ Week12LISTproject.py
@@ -2,6 +2,7 @@
 #by amending items, then print the list. 
 
 from operator import add
+from time import sleep
 
 
 AWSservices = []
@@ -14,10 +15,20 @@ AWSservices.append("Lambda")
 
 #Add 5 services to the list
 print (*AWSservices)
+sleep(1)
 print("The number of services in this list is:", len(AWSservices))
+
+sleep(1)
+
+print ("Removing 2 items from the list...")
+sleep(2)
+print ("Still computing this complex code...")
+sleep(3)
 
 #Delete the first 2 services from the list
 del AWSservices[0]
 del AWSservices[0]
 
 print (*AWSservices)
+sleep(1)
+print ("The length of the list is now:", len(AWSservices))
