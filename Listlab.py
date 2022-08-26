103fdced55eedceb7587efcf5f33c1dcc20d463d
Chad Bates <cbates255@gmail.com>
2022-08-25 21:13:26 -0400

    Completed daily lab

---
+++ Listlab.py
@@ -0,0 +1,13 @@
+users = []
+
+users.append("kevin")
+users.append("bob")
+users.append("alice")
+
+print(users)
+
+print (list(reversed(users)))
+
+users += ("andy", "wanda", "jim")
+
+print(users[3:6])
