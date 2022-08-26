a6462bcfdb3b1ebee8cc9ae73f6d2298de283117
Chad Bates <cbates255@gmail.com>
2022-08-26 16:36:04 -0400

    Completed lab with strings and methods!

--- 8-26stringslab.py
+++ 8-26stringslab.py
@@ -6,3 +6,12 @@ print('Lowercase:', message.lower())
 print('Uppercase:', message.upper())
 print('Capitaliszed:', message.capitalize())
 print('Title Case:', message.title())
+
+#Separating the words from the message
+words = message.split()
+print('Words:', words)
+
+#Sorting out the words from the message alphabetically
+sorted_words = sorted(words)
+print('Alphabetic First Word: ', sorted_words[0])
+print('Alphabetic Last Word: ', sorted_words[-1])
