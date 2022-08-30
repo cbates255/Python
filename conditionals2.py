9a6697497625d5fc6a296e9d24622ccdaadcd538
Chad Bates <108299323+cbates255@users.noreply.github.com>
2022-08-30 15:47:25 -0400

    Continued practice on conditionals2

--- conditionals2.py
+++ conditionals2.py
@@ -1,1 +1,11 @@
 value = int(input("Enter a number: "))
+
+if value % 5 == 0 and value % 3 == 0:
+    print("Fizzbuzz")
+elif value % 5 == 0:
+    print("Fizz")
+elif value % 5 == 0:
+    print("Buzz")
+else:
+    print(value)
+    
