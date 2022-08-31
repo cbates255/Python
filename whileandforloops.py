a32aa435d2c14e908aea14aa89cddb6ec5e74efe
Chad Bates <108299323+cbates255@users.noreply.github.com>
2022-08-30 21:08:00 -0400

    added some more for loops.

--- whileandforloops.py
+++ whileandforloops.py
@@ -6,4 +6,10 @@ while count < 5:
     print("Looping...")
     count += 1
     
+test_list = [1, 2, 3, 4, 5]
+for number in test_list:
+    print(number * 5)
     
+colors = ["red", "blue", "purple", "green", "yellow", "orange"]
+for color in colors:
+    print (color, "is a color.")
