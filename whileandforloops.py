1a685ee6448a2391d2214e601e1080d4d57768f0
Chad Bates <108299323+cbates255@users.noreply.github.com>
2022-08-30 21:21:28 -0400

    coded break and continue practice

--- whileandforloops.py
+++ whileandforloops.py
@@ -13,3 +13,12 @@ for number in test_list:
 colors = ["red", "blue", "purple", "green", "yellow", "orange"]
 for color in colors:
     print (color, "is a color.")
+    
+for color in colors:
+    if color == "red":
+        continue
+    elif color == "blue":
+        print(color)
+    elif color == "purple":
+        break
+    
