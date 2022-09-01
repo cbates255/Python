a3e9036012cae05276eece9112782994a0f5d251
Chad Bates <108299323+cbates255@users.noreply.github.com>
2022-08-31 21:20:57 -0400

    Continued work on functions!

--- FUNctions.py
+++ FUNctions.py
@@ -2,3 +2,12 @@ def print_name(name):
     print("Name is", name)
     
 print_name("Amber")
+
+def add(num1, num2):
+    return num1 + num2
+
+print (add(1, 5))
+
+def contact_card(name, age, car):
+    return f"{name} is {age} and drives a {car}."
+print(contact_card("Keith", 29, "Civic"))
