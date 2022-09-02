0cf7cf6c95937e05ece08eb62958fff6b6a65688
Chad Bates <108299323+cbates255@users.noreply.github.com>
2022-09-02 09:26:43 -0400

    Notes from coaching call

---
+++ coachcall9-1.py
@@ -0,0 +1,12 @@
+li_string = "This is a long string with a lot of random words in it that don't mean anything"
+li_list = li_string.split()
+
+def check_list_for_string(s, l, case_sensitive = False):
+    if (not case_sensitive):
+        s = s.strip().lower()
+        l = [st.strip().lower() for st in l]
+  
+    if (s in l):
+        print(s + " is in")      
+    else:
+        print(s + " is not in")
