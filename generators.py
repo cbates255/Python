d379494d1dc725f07105379d34f3d951f26df216
Chad Bates <cbates255@gmail.com>
2022-09-02 20:09:51 -0400

    Generator practice

--- generators.py
+++ generators.py
@@ -4,4 +4,16 @@ def gen_range(stop, start=1, step=1):
         yield num
         num += step
 
-#yield is kind of like return
+#yield is kind of like return. function will stop here until the generator
+#is called again. 
+
+for num in gen_range(10, step=2):
+    print(num)
+
+#This is a fibonacci generator
+def gen_fib():
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a
+        
