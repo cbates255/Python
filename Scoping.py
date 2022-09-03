191429921130a87d7560b2def109841d8cecbbd3
Chad Bates <cbates255@gmail.com>
2022-09-02 20:40:55 -0400

    Scoping practice

---
+++ Scoping.py
@@ -0,0 +1,12 @@
+#variables created within a function (def)..... >>> remain inside the function.
+#The variable will be restricted to the scope of the function
+
+if 1 < 2:
+    x = 5
+
+while x < 6:
+    print(x)
+    x += 1
+
+print(x)
+
