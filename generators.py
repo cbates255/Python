96dff3cbb1da170274b78f0e2ed6822769f74d52
Chad Bates <cbates255@gmail.com>
2022-09-02 20:26:25 -0400

    Finished generator practice

--- generators.py
+++ generators.py
@@ -1,3 +1,6 @@
+from multiprocessing.spawn import import_main_path
+import random
+
 def gen_range(stop, start=1, step=1):
     num = start
     while num <= stop:
@@ -16,4 +19,17 @@ def gen_fib():
     while True:
         a, b = b, a + b
         yield a
-        
+
+def char_range(start, stop, step=1):
+    stop_modifier = 1
+    start_code = ord(start)
+    stop_code = ord(stop)
+
+    if start_code > stop_code:
+        step *= -1
+        stop_modifier *= -1
+
+    for value in range(start_code, stop_code + stop_modifier, step):
+        yield chr(value)
+
+print (list(char_range("a", "g", 1)))
