ed183c4102e008750e403cd00d2a3dfa209c7794
Chad Bates <cbates255@gmail.com>
2022-09-02 23:07:15 -0400

    Addtl project work

--- week13project.py
+++ week13project.py
@@ -6,13 +6,12 @@ import string
 
 counter = 1
 
-letters = random.choices(string.ascii_letters, k=3)
-letterlist = "".join(letters)
-numbers = random.sample(range(100, 999), k=1)
-
 how_many = int(input("How many EC2's will be created? "))
 dept = input("What is your department? ")
 
 while counter <= how_many:
-    print(dept+'-'+letterlist+numbers)
+    letters = random.choices(string.ascii_letters, k=3)
+    letterlist = "".join(letters)
+    numbers = random.sample(range(100, 999), k=1)
+    print (dept+'-'+letterlist+str(*numbers))
     counter += 1
