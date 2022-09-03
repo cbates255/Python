06560289ddda88f5c78a6729f344a00fb230b4e6
Chad Bates <cbates255@gmail.com>
2022-09-02 23:32:35 -0400

    Advanced complete

--- week13project.py
+++ week13project.py
@@ -5,13 +5,18 @@ import random
 import string
 
 counter = 1
+allowed = ['marketing', 'Marketing', 'Finops', 'FinOps', 'Accounting' 'finops', 'finOps', 'accounting']
 
 how_many = int(input("How many EC2's will be created? "))
 dept = input("What is your department? ")
 
-while counter <= how_many:
-    letters = random.choices(string.ascii_letters, k=3)
-    letterlist = "".join(letters)
-    numbers = random.sample(range(100, 999), k=1)
-    print (dept+'-'+letterlist+str(*numbers))
-    counter += 1
+if dept not in allowed:
+    print('This generator is restricted to specified departments only. All other departments are forbidden')
+
+else:
+    while counter <= how_many:
+        letters = random.choices(string.ascii_letters, k=3)
+        letterlist = "".join(letters)
+        numbers = random.sample(range(100, 999), k=1)
+        print (dept+'-'+letterlist+str(*numbers))
+        counter += 1
