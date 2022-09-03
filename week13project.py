d260a422261af524f0d629c9529058bde8c0020e
Chad Bates <cbates255@gmail.com>
2022-09-02 22:46:34 -0400

    multiple changes to generator

--- week13project.py
+++ week13project.py
@@ -1,10 +1,18 @@
 #This project will create an EC2 name generator
+from itertools import count
 from ntpath import join
 import random
 import string
 
-how_many = input("How many EC2's will be created? ")
+counter = 1
+
+letters = random.choices(string.ascii_letters, k=3)
+letterlist = "".join(letters)
+numbers = random.sample(range(100, 999), k=1)
+
+how_many = int(input("How many EC2's will be created? "))
 dept = input("What is your department? ")
 
-
-
+while counter <= how_many:
+    print(dept+'-'+letterlist+numbers)
+    counter += 1
