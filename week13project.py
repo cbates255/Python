c2d041d149d920afe9949c6e0cf54eb66c743797
Chad Bates <cbates255@gmail.com>
2022-09-02 21:24:03 -0400

    Project work

--- week13project.py
+++ week13project.py
@@ -1,1 +1,10 @@
 #This project will create an EC2 name generator
+from ntpath import join
+import random
+import string
+
+how_many = input("How many EC2's will be created? ")
+dept = input("What is your department? ")
+
+
+
