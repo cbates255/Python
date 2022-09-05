eccd910347fe52020e7cd3c7e8f05a0e21def123
Chad Bates <cbates255@gmail.com>
2022-09-05 14:22:01 -0400

    Fixed bucket code

--- boto3s3.py
+++ boto3s3.py
@@ -1,12 +1,12 @@
 #Practice creating an S3 bucket with boto3
 
 import boto3
-aws_resource = boto3.resource("s3")
-bucket = aws_resource.Bucket("testBucket")
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('testing784512')
 response = bucket.create(
     ACL='public-read',
     CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-1'
+        'LocationConstraint': 'us-east-2'
     },
 )
 
