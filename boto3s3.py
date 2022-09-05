c7f54f1fdc2f6102f778931446325f858602f071
Chad Bates <cbates255@gmail.com>
2022-09-05 13:54:16 -0400

    s3 create bucket boto3

--- boto3s3.py
+++ boto3s3.py
@@ -1,4 +1,13 @@
 #Practice creating an S3 bucket with boto3
 
 import boto3
+aws_resource = boto3.resource("s3")
+bucket = aws_resource.Bucket("testBucket")
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-1'
+    },
+)
 
+print(response)
