0912a59e695f7b817a9f256e96762c91e6f03d8f
Chad Bates <cbates255@gmail.com>
2022-09-06 10:39:32 -0400

    Coded multiple files to bucket at once

--- boto3/boto3uploadobject.py
+++ boto3/boto3uploadobject.py
@@ -1,4 +1,15 @@
 import boto3
 #Upload a single file to S3 bucket. 
-s3_resource = boto3.client("s3")
-s3_resource.upload_file(Bucket = 'testing784512', Filename = 'Listlab.py', Key='testupload')
+#s3_resource = boto3.client("s3")
+#s3_resource.upload_file(Bucket = 'testing784512', Filename = 'Listlab.py', Key='testupload')
+
+#Upload multiple files to bucket. 
+#import os
+import glob
+#cwd=os.getcwd()
+
+files=glob.glob('*.py')
+
+for file in files:
+    s3_resource = boto3.client("s3")
+    s3_resource.upload_file(Bucket = 'testing784512', Filename = file, Key=file)
