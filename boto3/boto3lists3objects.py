64b257434a877ca5588dfee8f0d123ce2498841c
Chad Bates <cbates255@gmail.com>
2022-09-06 11:22:18 -0400

    Coded lists, length.

--- boto3/boto3lists3objects.py
+++ boto3/boto3lists3objects.py
@@ -1,2 +1,10 @@
 import boto3
 
+s3_resource=boto3.client("s3")
+#s3_resource.list_objects(Bucket='testing784512')
+#This will provide a list of the objects in the bucket.
+
+objects=s3_resource.list_objects(Bucket='testing784512')['Contents']
+print(len(objects))
+#The above gives the number of objects in the bucket. 
+
