4e4fee4a7971d256d30fc05035d3af16618ca147
Chad Bates <cbates255@gmail.com>
2022-09-07 14:31:23 -0400

    folder

--- week14projectadvanced.py
+++
@@ -1,9 +0,0 @@
-#Script to stop specified running EC2 instances.
-import boto3
-
-ec2=boto3.resource("ec2")
-
-
-ec2_filter=[{'Name': 'tag:Environment', 'Values': ['Dev']}, {'Name': 'instance-state-name', 'Values': ['running']}]
-
-ec2.instances.filter(Filters=ec2_filter).stop()
