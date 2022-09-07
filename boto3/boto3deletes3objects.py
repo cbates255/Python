f3bbc7d1e13570fda5cdb36a63abc96781dad0f1
Chad Bates <cbates255@gmail.com>
2022-09-06 21:05:28 -0400

    Coded

--- boto3/boto3deletes3objects.py
+++ boto3/boto3deletes3objects.py
@@ -1,4 +1,22 @@
+from ast import Del
 import boto3
 #Delete objects from S3 bucket.
-s3_resource=boto3.client("s3")
-s3_resource.delete_object(Bucket='testing784512', Key='Listlab.py')
+s3=boto3.client("s3")
+s3.delete_object(Bucket='testing784512', Key='Listlab.py')
+
+#Delete multiple objects
+
+import glob
+files=glob.glob('*.py')
+
+response = s3.delete_objects(
+    Bucket='testing784512',
+    Delete={
+        'Objects': [
+            {
+                'Key': 'bio.py',
+            },
+        ],
+    },
+)
+
