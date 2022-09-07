49c0b50dfd2d83a61b7b491f112ec6438ca990d7
Chad Bates <cbates255@gmail.com>
2022-09-07 14:26:53 -0400

    coded

--- boto3/boto3describevpcs.py
+++ boto3/boto3describevpcs.py
@@ -1,6 +1,12 @@
+from cgitb import handler
 import boto3
 vpc=boto3.client("ec2")
 
 x=vpc.describe_vpcs()
-print(len(x))
-print(x)
+
+vpcnumber=x["Vpcs"]
+
+#get vpc id out 
+
+for i in vpcnumber:
+    print(i["VpcId"])
