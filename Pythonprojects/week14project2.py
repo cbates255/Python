11c21cca9d8addc448279e883126c725ad13fb10
Chad Bates <cbates255@gmail.com>
2022-09-07 14:55:21 -0400

    created table

--- Pythonprojects/week14project2.py
+++ Pythonprojects/week14project2.py
@@ -1,3 +1,30 @@
 import boto3
 dynamodb=boto3.client("dynamodb")
 
+dynamodb.create_table(
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'Player',
+            'AttributeType': 'S',
+        },
+        {
+            'AttributeName': 'Number',
+            'AttributeType': 'N',
+        },
+    ],
+    KeySchema=[
+        {
+            'AttributeName': 'Player',
+            'KeyType': 'HASH',
+        },
+        {
+            'AttributeName': 'Number',
+            'KeyType': 'RANGE',
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5,
+    },
+    TableName='Player_list',
+)
