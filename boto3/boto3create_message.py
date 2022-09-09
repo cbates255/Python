d546df55c5f8d8d99df825facb24a6c605333424
Chad Bates <cbates255@gmail.com>
2022-09-09 15:05:47 -0400

    Coded

--- boto3/boto3create_message.py
+++ boto3/boto3create_message.py
@@ -3,6 +3,8 @@ import boto3
 sqs=boto3.client("sqs")
 
 response = sqs.send_message(
-    QueueUrl='string',
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/913213447178/Callqueue',
     MessageBody='Message sent from Python',
 )
+
+print('Message sent')
