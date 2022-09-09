73bb71d6e0dfa327a4218d27ae9c3815f2b19601
Chad Bates <cbates255@gmail.com>
2022-09-09 15:22:37 -0400

    get messages from queue

--- boto3/boto3sqs_get_messages.py
+++ boto3/boto3sqs_get_messages.py
@@ -1,6 +1,12 @@
 import boto3
+import pprint
 sqs=boto3.client("sqs")
 
-response = client.receive_message(
+response = sqs.receive_message(
     QueueUrl='https://queue.amazonaws.com/913213447178/Callqueue',
 )
+
+messages=response['Messages']
+for message in messages:
+    data=message['Body']
+    print(data)
