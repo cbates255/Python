fa6064563f4be6f9d1343dad3d665c855668b7e4
Chad Bates <cbates255@gmail.com>
2022-09-10 20:37:25 -0400

    coded for snapshot

--- boto3/boto3ebssnapshot.py
+++ boto3/boto3ebssnapshot.py
@@ -4,5 +4,16 @@ import pprint
 ebs=boto3.client("ec2")
 
 volumes=ebs.describe_volumes()
+data=volumes['Volumes']
 
-pprint.pprint(volumes)
+volumeids=[]
+
+for x in data:
+    ids=x['VolumeId']
+    volumeids.append(ids)
+
+myid=volumeids[2]
+
+ebs.create_snapshot(VolumeId=myid, Description='Snapshot from base volume')
+
+print('Snapshot has been created')
