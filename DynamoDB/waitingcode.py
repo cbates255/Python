f53a6406dd1e8782c77cf3d221379044c534f739
Chad Bates <cbates255@gmail.com>
2022-09-13 10:48:09 -0400

    coded

--- DynamoDB/waitingcode.py
+++ DynamoDB/waitingcode.py
@@ -1,2 +1,13 @@
 #Code to wait for table status to go active before printing 'table active'
 
+from msilib import Table
+import boto3
+db=boto3.resource('dynamodb')
+
+db.create_table(TableName='Movies',)
+
+print('Table status:', db.table_status)
+
+print('Waiting for', db.name, 'to complete creating...')
+db.meta.client.get_waiter('table_exists').wait(TableName='Movies')
+print('Table status:', db.Table('Movies').table_status)
