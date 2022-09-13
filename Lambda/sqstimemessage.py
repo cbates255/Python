3d4e9a0254f8d5b30844ab5e83a5471df8e64e40
Chad Bates <cbates255@gmail.com>
2022-09-13 12:05:55 -0400

    send message

--- Lambda/sqstimemessage.py
+++ Lambda/sqstimemessage.py
@@ -1,16 +1,18 @@
 import boto3
-import time
+import datetime
 
 q = boto3.client('sqs')
-
+stamp = datetime.datetime.strftime("%Y-%m-%d %H:%M:%S")
+print(stamp)
+"""
 response = q.list_queues()
 queueurl = response['QueueUrls']
 print('Sending message...')
 
 message = q.send_message(
     QueueUrl=queueurl,
-    MessageBody='Week 15 project message',
+    MessageBody='The current time is' + stamp
 )
 
 print('The message has been sent to', queueurl)
-
+"""
