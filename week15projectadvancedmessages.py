cc521bbfe01b5337f3fed2bd33ca28988bfadc2c
Chad Bates <cbates255@gmail.com>
2022-09-14 15:59:41 -0400

    coded

--- week15projectadvancedmessages.py
+++ week15projectadvancedmessages.py
@@ -3,11 +3,11 @@ import boto3
 sns = boto3.client('sns')
 
 def lambda_handler(event, context):
-    for message in event['Records']['body']:
-        print('The message is: ' + message)
+    message = event['Records'][0]['body']
+    print('The message is: ' + message)
 
-    response = sns.publish(
-    Message='In case you weren\'t aware, ' + message
+    sns.publish(
+    Message='In case you weren\'t aware, ' + message,
     Subject='Week 15 project!',
     TopicArn='arn:aws:sns:us-east-1:913213447178:week15sns'
     )
